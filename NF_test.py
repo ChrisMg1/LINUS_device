64c888ba8098101208d621cb4965e4891bceaf71
ChrisMg1 <christoph.maget@outlook.com>
2022-02-27 20:30:38 +0000

    verbosing

--- NF_test.py
+++ NF_test.py
@@ -9,51 +9,50 @@ from scapy.all import *
 
 
 def print_and_accept(pkt):
+    print('==> CM: IPtables packet')
     print(pkt)
 # Here I 'convert' nfqueue to scapy:
-    print('CM:Do fancy stuff')
     sc_pkt = IP(pkt.get_payload())
-    print ( sc_pkt, type(sc_pkt) )
+    print('==> CM: scapy packet begin')
+    print ( sc_pkt.show())
+    print(type(sc_pkt) )
     for byte in sc_pkt:
         pass
 #        print( byte )
 
-    print('DST', sc_pkt.dst )
-    print('SRC', sc_pkt.src )
-
-    print('looking into IP')
-    print(sc_pkt.show())
-    print('IP checksum: ', sc_pkt[IP].chksum)
+#    print('DST', sc_pkt.dst )
+#    print('SRC', sc_pkt.src )
+#    print('CHK', sc_pkt.chksum)
+#    print('IP checksum: ', sc_pkt[IP].chksum)
 
 # TCP payload is the actual data to be encrypted
     if sc_pkt.haslayer(TCP):
-        print('TCP checksum: ', sc_pkt[TCP].chksum)
-        print('PAYLOAD before: ', sc_pkt[TCP].payload, type(sc_pkt[TCP].payload))
         payload_before = len(sc_pkt[TCP].payload)
-#        sc_pkt[TCP].payload = sc_pkt.raw(str(sc_pkt[TCP].payload).replace('err','txx'))
-# HEUREKA! This seems to be how to change the 'raw'-payload in TCP-segment:
 
-        if sc_pkt.haslayer(Raw):
-            sc_pkt[Raw].load = 'cm_replace\n'
-            del sc_pkt.chksum
-            sc_pkt = sc_pkt.__class__(bytes(sc_pkt))
+    if sc_pkt.haslayer(Raw):
+        sc_pkt[Raw].load = 'cm_re\n'
+        del sc_pkt.chksum
+        sc_pkt = sc_pkt.__class__(bytes(sc_pkt))
 
         payload_after = len(sc_pkt[TCP].payload)
         payload_dif = payload_after - payload_before
         sc_pkt[IP].len = sc_pkt[IP].len + payload_dif
 #        sc_pkt[TCP].payload = bytes('ex\n', 'UTF-8')
 
-#TODO: Here seems to be the problem: The packet is not properly crafted after 'replace'; confusing byte / str when trying 'print(sc_pkt.show())'
-        print('PAYLOAD after: ', sc_pkt[TCP].payload, type(sc_pkt[TCP].payload))
 
-#        pkt.set_payload(bytes(sc_pkt))
+        print('==> CM: scapy packet end')
+        print ( sc_pkt.show())
+
+# TODO: The packet is crafted, but how to send the manipulated packet??
+
 
 #TODO: Either use nfqueue (...verdict... seems deprecated) or scapy (drop/send seems have trouble with packet format str/byte)
 #        pkt.set_verdict_modified(nfqueue.NF_ACCEPT, str(sc_pkt), len(sc_pkt))
-#        pkt.drop()
-#        send(sc_pkt)
 
-# TODO: The packet is crafted, but how to send the manipulated packet??
+
+#        pkt.drop()
+#        sendp(sc_pkt)
+
         pkt.accept()
 
     else:
