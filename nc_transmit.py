d5d3eeb568a71738c3102590f792c842e5cac75f
ChrisMg1 <christoph.maget@outlook.com>
2022-05-27 08:43:36 +0200

    details

--- nc_transmit.py
+++ nc_transmit.py
@@ -5,7 +5,9 @@ Created on Thu May 26 15:07:02 2022
 @author: chris
 """
 
+# All parameters are stored in external file (not on github)
 import config
+
 import socket
 import sys
 import time
@@ -17,9 +19,11 @@ static = True
 if (static):
     hostname = config.dest_uri
     port = config.dest_port
+    print_out = config.to_print
 else:
     hostname = sys.argv[1]
     port = int(sys.argv[2])
+    print_out = sys.argv[3]
 
 def nc_transmit(hn, p, content):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,4 +34,4 @@ def nc_transmit(hn, p, content):
     s.close()
 
 
-nc_transmit(config.dest_uri, config.dest_port, config.to_print)
+nc_transmit(hostname, port, print_out)
